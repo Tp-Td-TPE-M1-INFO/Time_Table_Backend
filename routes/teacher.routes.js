dd75519ed34d2f1631064fd89cfffcfd2d2936cf
Jocelynpyw <82529390+Jocelynpyw@users.noreply.github.com>
2023-07-12 04:16:30 +0100

    Update teacher.routes.js
    ajout du params a la fin de la route pour delete un teacher

--- routes/teacher.routes.js
+++ routes/teacher.routes.js
@@ -7,7 +7,7 @@ router.post('/teacher/register', addTeacher);
 router.get('/teacher/info/:id', getTeacher)
 router.get('/teacher/all', getAllTeachers)
 router.patch('/teacher/update', updateTeacher)
-router.delete('/teacher/delete', deleteTeacher)
+router.delete('/teacher/delete/:id', deleteTeacher)
 router.patch('/teacher/profil',upload, profil)
 router.patch('/teacher/deleteProfil',deleteProfil)
 
