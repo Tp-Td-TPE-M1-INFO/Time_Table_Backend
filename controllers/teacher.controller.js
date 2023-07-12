963763226e6d0bfc38415fd5ffb46334bb35856c
Jocelynpyw <82529390+Jocelynpyw@users.noreply.github.com>
2023-07-12 04:20:24 +0100

    Update teacher.controller.js
    ajustement de la fonction delete un teacher

--- controllers/teacher.controller.js
+++ controllers/teacher.controller.js
@@ -2,6 +2,8 @@ const Teacher = require("../models/teacher.model");
 const { registerValidation } = require("../middlewares/validation");
 const bcrypt = require("bcrypt");
 const errorCtr = require("../utils/error.utils");
+const asyncHandler = require('express-async-handler');
+
 
 //Check if the registration number is already in the database
 const addTeacher = async (req, res) => {
@@ -67,14 +69,20 @@ const updateTeacher = async (req, res) => {
   }
 };
 
-const deleteTeacher = async (req, res) => {
-  try {
-    await Teacher.deleteOne(req.teacher._id);
-    res.status(200).json({ message: "teacher deleted" });
-  } catch (err) {
-    res.status(400).json({ message: err });
-  }
-};
+
+//Delete Teacher 
+const deleteTeacher = asyncHandler(async (req,res) => {
+    const classe = await Teacher.findByIdAndDelete(req.params.id);
+    if(!classe){
+        res.status(404);
+        throw new Error('teacher not found');
+    };
+    // await Class.deleteOne();
+    res.status(200).json({message: "Teacher deleted"})
+});
+
+
+
 
 const getAllTeachers = async (req, res) => {
   try {
